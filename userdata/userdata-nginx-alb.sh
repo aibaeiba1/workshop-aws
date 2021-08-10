1f11a2b3b10de2350aa6b867239e747bc8e1a512
cisdthki@th.ibm.com <cisdthki@th.ibm.com>
2021-08-10 21:46:21 +0700

    rename userdata

--- userdata/userdata-nginx-alb.sh
+++ userdata/userdata-nginx-alb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum install -y amazon-linux-extras
-sudo amazon-linux-extras -y install nginx1
+sudo yum install nginx
 sudo mkdir -p /usr/share/nginx/html/web1
 sudo mkdir -p /usr/share/nginx/html/web2
 sudo echo "WEB1" | sudo tee /usr/share/nginx/html/web1/index.html
