29bfbe2038dc3efc17cda83cb16e89c3d526b168
cisdthki@th.ibm.com <cisdthki@th.ibm.com>
2021-08-10 21:49:36 +0700

    update userdata

--- userdata/userdata-nginx-alb.sh
+++ userdata/userdata-nginx-alb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum install -y amazon-linux-extras
-sudo yum install nginx
+sudo amazon-linux-extras install -y nginx1
 sudo mkdir -p /usr/share/nginx/html/web1
 sudo mkdir -p /usr/share/nginx/html/web2
 sudo echo "WEB1" | sudo tee /usr/share/nginx/html/web1/index.html
